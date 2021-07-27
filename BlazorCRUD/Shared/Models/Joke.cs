56359dee65cdb4b334943ee3f0b576da6a6c839d
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2021-07-27 18:17:56 +0200

    Added fetching jokes from external API, better details and index page

---
+++ BlazorCRUD/Shared/Models/Joke.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Shared.Models
+{
+    public class Joke
+    {
+        public string joke { get; set; }
+    }
+}
