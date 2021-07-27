e0ce8ee8a126bbc7054a7a3b53e82b0e29aceb2c
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2021-07-27 19:47:36 +0200

    Seed database posts

--- BlazorCRUD/Server/Data/ApplicationDbContext.cs
+++ BlazorCRUD/Server/Data/ApplicationDbContext.cs
@@ -12,9 +12,22 @@ namespace BlazorCRUD.Server.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-
+            
         }
 
         public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasData(new List<Post>
+                    {
+                        new Post { Id = 1, Title = "Post Title 1", Text = "Post text 1"},
+                        new Post { Id = 2, Title = "Post Title 2", Text = "Post text 2"},
+                        new Post { Id = 3, Title = "Post Title 3", Text = "Post text 3"},
+                        new Post { Id = 4, Title = "Post Title 4", Text = "Post text 4"},
+                        new Post { Id = 5, Title = "Post Title 5", Text = "Post text 5"}
+                    });
+        }
     }
 }
---
+++ BlazorCRUD/Server/Migrations/20210727174520_SeedPosts.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorCRUD.Server.Migrations
+{
+    public partial class SeedPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Text", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Post text 1", "Post Title 1" },
+                    { 2, "Post text 2", "Post Title 2" },
+                    { 3, "Post text 3", "Post Title 3" },
+                    { 4, "Post text 4", "Post Title 4" },
+                    { 5, "Post text 5", "Post Title 5" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
